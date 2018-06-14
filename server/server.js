01981d5ed4c6e9d288a1e1f5f018a5f129e31ec0
Mike Sausa <mikejsausa@gmail.com>
2018-06-13 20:37:49 -0500

    added test cases for delete /todos/:id

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       res.status(404).send();
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }), (err) => {
     res.status(400).send();
   }
