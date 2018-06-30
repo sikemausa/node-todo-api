c5f51fa5a2641e8fbddbf714de0006482ceb6ff4
Mike Sausa <mikejsausa@gmail.com>
2018-06-30 13:05:11 -0500

    add POST users/login route

--- server/server.js
+++ server/server.js
@@ -111,7 +111,19 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   let token = req.header('x-auth');
   res.send(req.user);
-})
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
